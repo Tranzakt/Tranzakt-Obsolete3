62b7d5858f153b40ee07975001a4ce1de67dba1e
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2022-08-16 20:25:17 +0100

    Extend migrations to submodules

--- src/tranzakt/Developer/Core/app/Providers/DeveloperServiceProvider.php
+++ src/tranzakt/Developer/Core/app/Providers/DeveloperServiceProvider.php
@@ -12,5 +12,14 @@ class DeveloperServiceProvider extends ServiceProvider
 	
 	public function boot()
 	{
+        if ( $this->app->runningInConsole() )
+        {
+            // $this->mergeConfigFrom(
+            //     __DIR__ . '/../config/laravel-migration-paths.php',
+            //     'laravel-migration-paths'
+            // );
+
+			$this->loadMigrationsFrom(SubmoduleDirs(from: __DIR__ . '/../../..', subdir: 'database/migrations'));
+        }
 	}
 }
